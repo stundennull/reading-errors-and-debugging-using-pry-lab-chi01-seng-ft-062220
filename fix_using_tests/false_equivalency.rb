0648e10acf65ae9c141215413a810ce285c3012b
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-15 00:10:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -12,10 +13,9 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+   "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
-  end
+   "HAM HAM HAM IN MY TUMMY"
 end
 
 def runner
